89806ae4b684f4f32d547b2e9a32041de12fa89c
Phareedah Abdul <fariffarida@gmail.com>
2026-09-12 14:33:22 +0100

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@ const mainStore = useMainStore();
         <img
           src="/my-picture.png"
           alt="Sample Image"
-          class="rounded-full mr-5 w-full h-full"
+          class="rounded-full mr-5 w-[50%] h-[50%]"
         />
       </div>
     </div>
